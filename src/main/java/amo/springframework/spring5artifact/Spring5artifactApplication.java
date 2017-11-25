b69815e5497b59b6033168fe365452f678b1dfd7
JavaGalaxy <amogh.shendre@gmail.com>
2017-11-24 22:21:16 -0600

    Initial Commit 11.24.17

---
+++ src/main/java/amo/springframework/spring5artifact/Spring5artifactApplication.java
@@ -0,0 +1,12 @@
+package amo.springframework.spring5artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5artifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5artifactApplication.class, args);
+	}
+}
